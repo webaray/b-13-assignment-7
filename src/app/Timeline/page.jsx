a9f150f921385380652ca908bc0aa32158bab090
webaray <josimuddin105464@gmail.com>
2026-04-15 02:45:32 +0600

    Project insatial setup and navebar create

---
+++ src/app/Timeline/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TimelinePage = () => {
+    return (
+        <div>
+            <h2>This is the Timeline Page</h2>
+        </div>
+    );
+};
+
+export default TimelinePage;
---
+++ src/app/stats/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StatsPage = () => {
+    return (
+        <div>
+            <h2>This is the Stats Page</h2>
+        </div>
+    );
+};
+
+export default StatsPage;
