7b9ab47454fc9821cd4e44379e50e02814d0ec66
webaray <josimuddin105464@gmail.com>
2026-04-15 03:08:09 +0600

    Add hero section

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2 className="text-5xl font-bold">This is Home page</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="">
+          <h1 className="text-5xl font-bold">Friends to keep close in your life</h1>
+          <p className="py-6">
+            Your personal shelf of meaningful connections. Browse, tend, and nurture the <br/>
+            relationships that matter most.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
